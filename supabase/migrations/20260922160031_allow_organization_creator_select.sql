3aea9473a8030df429e1e5b6b03fd64225032313
Johan Kindgren <johan.kindgren@gmail.com>
2026-09-22 18:01:11 +0200

    Allow organization creator to read inserted row

---
+++ supabase/migrations/20260922160031_allow_organization_creator_select.sql
@@ -0,0 +1,11 @@
+drop policy if exists "members can read their organizations"
+  on public.organizations;
+
+create policy "members and creators can read their organizations"
+  on public.organizations
+  for select
+  to authenticated
+  using (
+    public.is_organization_member(id)
+    or created_by = (select auth.uid())
+  );
